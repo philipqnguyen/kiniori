a97911621cbea41123163e99c6887025f3b50d83
Philip Nguyen <supertaru@gmail.com>
2015-09-12 15:15:12 -0700

    rename bookmark to bookmarkItem

--- client/templates/bookmarks/bookmark_item.js
+++ client/templates/bookmarks/bookmark_item.js
@@ -1,6 +1,5 @@
 // Template.bookmark.helpers({
 //   url: function () {
-//     console.log('url helper')
 //     bookmark = Bookmarks.findOne({bookmarkId: this._id});
 //     return bookmark.url;
 //   }
